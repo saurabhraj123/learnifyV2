57e2199ebd6952481cb43f6b2c9ca97b77d134c4
saurabhraj123 <me4saurabh4u@gmail.com>
2024-03-25 13:59:30 +0530

    Play video on course page

--- app/courses/[slug]/VideoPlayer.tsx
+++ app/courses/[slug]/VideoPlayer.tsx
@@ -1,13 +1,34 @@
+"use client";
 import { MediaPlayer, MediaProvider } from "@vidstack/react";
 import {
   defaultLayoutIcons,
   DefaultVideoLayout,
 } from "@vidstack/react/player/layouts/default";
+import { set, get } from "idb-keyval";
+import { useEffect, useState } from "react";
 
 interface Props {
-  src: string;
+  fileName: string;
+  courseName: string;
+  sectionName: string;
 }
-const VideoPlayer = ({ src }: Props) => {
+
+const VideoPlayer = ({ fileName, sectionName, courseName }: Props) => {
+  const [src, setSrc] = useState<any>("");
+
+  async function updateSrc() {
+    const dirHandle = await get(courseName);
+    const sectionHandle = await dirHandle.getDirectoryHandle(sectionName);
+    const fileHandle = await sectionHandle.getFileHandle(fileName);
+    const file = await fileHandle.getFile();
+    const url = URL.createObjectURL(file);
+    setSrc({ src: url, type: "video/mp4" });
+  }
+
+  useEffect(() => {
+    updateSrc();
+  }, []);
+
   return (
     <MediaPlayer title="Sprite Fight" aspectRatio="16/9" src={src} autoPlay>
       <MediaProvider />
--- app/courses/[slug]/[resourceId]/page.tsx
+++ app/courses/[slug]/[resourceId]/page.tsx
@@ -1,12 +1,43 @@
+import authOptions from "@/app/api/auth/authOptions";
+import prisma from "@/prisma/client";
 import { Flex } from "@radix-ui/themes";
+import { getServerSession } from "next-auth";
 import React from "react";
+import VideoPlayer from "../VideoPlayer";
 
 interface Props {
   params: { resourceId: string };
 }
 
-const ResourcePage = ({ params }: Props) => {
-  return <Flex mx="5">{params.resourceId}</Flex>;
+const ResourcePage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions);
+  if (!session) return <div>Please login to continue...</div>;
+
+  const resource = await prisma.file.findFirst({
+    where: {
+      slug: params.resourceId,
+      section: { course: { userId: (session.user as any)?.id } },
+    },
+    include: {
+      section: {
+        include: {
+          course: true,
+        },
+      },
+    },
+  });
+
+  if (!resource) return <div>Resource not found</div>;
+
+  return (
+    <Flex mx="5">
+      <VideoPlayer
+        courseName={resource.section.course.title}
+        sectionName={resource.section.title}
+        fileName={resource.title}
+      />
+    </Flex>
+  );
 };
 
 export default ResourcePage;
